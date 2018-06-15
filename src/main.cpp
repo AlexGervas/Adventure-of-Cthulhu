611f305661e218a9ac89123bfecba02c0c902915
AleksKirienko <aleks.Kirienko@bk.ru>
2018-06-15 15:25:55 +0700

    Add menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 using namespace sf;
 using namespace std;
 
-void setFillColor(const Color& color);
-int ts = 56; 
+void setColor(const Color& color);
+int ts = 56;
 Vector2i offset(15, 15);
 int rec = 0;
 
@@ -27,7 +27,7 @@ int main()
 	RenderWindow app(VideoMode(1024, 512), "Cthulhu!");
 	app.setFramerateLimit(60);
 
-	Texture t1, t2, t3, t4, t5,t6;
+	Texture t1, t2, t3, t4, t5, t6;
 	t1.loadFromFile("images/back.png");
 	t2.loadFromFile("images/cthulh.png");
 	t3.loadFromFile("images/sprites.png");
@@ -35,7 +35,7 @@ int main()
 	t5.loadFromFile("images/heart.png");
 	t6.loadFromFile("images/coins.png");
 
-	Sprite Back(t1), Hero(t2), Enemy(t3), Fire(t4), Heart(t5), Coins (t6);
+	Sprite Back(t1), Hero(t2), Enemy(t3), Fire(t4), Heart(t5), Coins(t6);
 
 	Anti En;
 
@@ -49,125 +49,185 @@ int main()
 	const int N = 3;
 	int xc[N];
 	int yc[N];
+	int c = 1;
+
+	Text txt2, txt3;
+	txt2.setFont(font);
+	txt2.setCharacterSize(40);
+	txt2.setColor(Color::Blue);
+	txt2.setPosition(35, 35);
+	txt2.setString(string("1. New game"));
+	txt3.setFont(font);
+	txt3.setCharacterSize(40);
+	txt3.setColor(Color::Yellow);
+	txt3.setPosition(35, 50);
+	txt3.setString(string("\n2. Help"));
+
+	bool mar = 0;
 	for (int i = 0; i < N; i++)
 	{
 		xc[i] = rand() % 900 + 100;
 		yc[i] = rand() % 400 + 100;
 	}
-
-	while (app.isOpen())
+	while (mar != 1)
 	{
-		Event e;
-		Text txt, txt1;
-		txt.setFont(font);
-		txt.setCharacterSize(25);
-		txt.setColor(Color::Blue);
-		txt.setPosition(900, 30);
-		txt.setString(string("Score: \n      ") + to_string(rec));
-
-		txt1.setFont(font);
-		txt1.setCharacterSize(40);
-		txt1.setColor(Color::Red);
-		txt1.setPosition(125, 35);
-		txt1.setString(string("x") + to_string(life));
-
-		while (app.pollEvent(e))
+		if (c == 1) 
 		{
-			if (e.type == Event::Closed || life <= 0)
-				app.close();
+			txt2.setColor(Color::Blue); 
+			txt3.setColor(Color::Yellow); 
+		}
+		if (c == 2) 
+		{
+			txt3.setColor(Color::Blue);
+			txt2.setColor(Color::Yellow);
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Down)) y += 3;
-		if (Keyboard::isKeyPressed(Keyboard::Up)) y -= 3;
-		if (Keyboard::isKeyPressed(Keyboard::Right)) x += 3;
-		if (Keyboard::isKeyPressed(Keyboard::Left)) x -= 3;
-		if (Keyboard::isKeyPressed(Keyboard::Return))
-		{
-			xf += 5;
-			yf = y;
-			alphaF = 255;
-			mark = 1;
-		}
-		if (mark == 1) xf += 5;
+		app.draw(Back);
+		app.draw(txt2);
+		app.draw(txt3);
 
-		if (xf > 900)
+		app.display();
+		if (Keyboard::isKeyPressed(Keyboard::Down)) c = 2;
+		if (Keyboard::isKeyPressed(Keyboard::Up)) c = 1;
+		if (Keyboard::isKeyPressed(Keyboard::Return)) mar = 1;
+	}
+	if (c == 1)
+	{
+		while (app.isOpen())
 		{
-			mark = 0;
-			alphaF = 0;
-			xf = x;
-			yf = y;
-		}
-		if (En.x > 30)
-		{
-			En.x -= 2;
-		}
-		else
-		{
-			En.x = 800;
-			En.y = rand() % 400;
-			En.kind = rand() % 3;
-			En.match = En.kind + 1;
-		}
-		if (abs(En.x - xf) < 65 && abs(En.y - yf) < 65 && mark == 1)
-		{
-			En.match--;
-			//En.x += 80;
-			alphaF = 0;
-			xf = x;
-			yf = y;
-			if (En.match == 0)
+			Event e;
+			Text txt, txt1;
+			txt.setFont(font);
+			txt.setCharacterSize(25);
+			txt.setColor(Color::Blue);
+			txt.setPosition(900, 30);
+			txt.setString(string("Score: \n      ") + to_string(rec));
+
+			txt1.setFont(font);
+			txt1.setCharacterSize(40);
+			txt1.setColor(Color::Red);
+			txt1.setPosition(125, 35);
+			txt1.setString(string("x") + to_string(life));
+
+
+
+			while (app.pollEvent(e))
+			{
+				if (e.type == Event::Closed || life <= 0)
+					app.close();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) y += 3;
+			if (Keyboard::isKeyPressed(Keyboard::Up)) y -= 3;
+			if (Keyboard::isKeyPressed(Keyboard::Right)) x += 3;
+			if (Keyboard::isKeyPressed(Keyboard::Left)) x -= 3;
+			if (Keyboard::isKeyPressed(Keyboard::Return))
+			{
+				xf += 5;
+				yf = y;
+				alphaF = 255;
+				mark = 1;
+			}
+			if (mark == 1) xf += 5;
+
+			if (xf > 900)
 			{
 				mark = 0;
 				alphaF = 0;
 				xf = x;
 				yf = y;
+			}
+			if (En.x > 30)
+			{
+				En.x -= 2;
+			}
+			else
+			{
 				En.x = 800;
 				En.y = rand() % 400;
 				En.kind = rand() % 3;
 				En.match = En.kind + 1;
-				rec++;
 			}
-		}
-		if (En.x == x && abs(En.y - y) < 65)
-		{
-			life--;
-		}
-		for (int i = 0; i < N; i++)
-		{
-			if (abs(x - xc[i]) < 30 && abs(y - yc[i]) < 30)
+			if (abs(En.x - xf) < 65 && abs(En.y - yf) < 65 && mark == 1)
 			{
-				xc[i] = rand() % 900 + 100;
-				yc[i] = rand() % 400 + 100;
-				rec += 5;
+				En.match--;
+				//En.x += 80;
+				alphaF = 0;
+				xf = x;
+				yf = y;
+				if (En.match == 0)
+				{
+					mark = 0;
+					alphaF = 0;
+					xf = x;
+					yf = y;
+					En.x = 800;
+					En.y = rand() % 400;
+					En.kind = rand() % 3;
+					En.match = En.kind + 1;
+					rec++;
+				}
 			}
+			if (En.x == x && abs(En.y - y) < 65)
+			{
+				life--;
+			}
+			for (int i = 0; i < N; i++)
+			{
+				if (abs(x - xc[i]) < 30 && abs(y - yc[i]) < 30)
+				{
+					xc[i] = rand() % 900 + 100;
+					yc[i] = rand() % 400 + 100;
+					rec += 5;
+				}
+			}
+			Enemy.setTextureRect(IntRect(En.kind * 131, 0, 131, 131));
+			Enemy.setColor(Color(255, 255, 255, En.alpha));
+			Enemy.setPosition(En.x, En.y);
+
+			Fire.setColor(Color(255, 255, 255, alphaF));
+			Fire.setPosition(xf, yf);
+
+			Hero.setPosition(x, y);
+
+			Heart.setPosition(50, 30);
+
+			app.draw(Back);
+			for (int i = 0; i < N; i++)
+			{
+				Coins.setPosition(xc[i], yc[i]);
+				app.draw(Coins);
+			}
+			app.draw(Hero);
+			app.draw(Enemy);
+			app.draw(Fire);
+			app.draw(txt);
+			app.draw(Heart);
+			app.draw(txt1);
+
+
+			app.display();
 		}
-		Enemy.setTextureRect(IntRect(En.kind*131, 0, 131, 131));
-		Enemy.setColor(Color(255, 255, 255, En.alpha));
-		Enemy.setPosition(En.x, En.y);
-
-		Fire.setColor(Color(255, 255, 255, alphaF));
-		Fire.setPosition(xf, yf);
-
-		Hero.setPosition(x, y);
-
-		Heart.setPosition(50, 30);
-
-		app.draw(Back);
-		for (int i = 0; i < N; i++)
-		{
-			Coins.setPosition(xc[i], yc[i]);
-			app.draw(Coins);
-		}
-		app.draw(Hero);
-		app.draw(Enemy);
-		app.draw(Fire);
-		app.draw(txt);
-		app.draw(Heart);
-		app.draw(txt1);
-
-		
-		app.display();
 	}
-
+	if (c == 2)
+	{
+		while (app.isOpen())
+		{
+			Event e;
+			while (app.pollEvent(e))
+			{
+				if (e.type == Event::Closed || life <= 0)
+					app.close();
+			}
+			app.draw(Back);
+			txt2.setFont(font);
+			txt2.setCharacterSize(20);
+			txt2.setColor(Color::Red);
+			txt2.setPosition(125, 35);
+			txt2.setString(string("Press w or s to move. Press enter to shoot \n Game rules : \n 1) The player gains points.Actions for which the account is increased : \n a) for every second of the game, 1 point is awarded  \n b) For one killed peasant is credited with 20 point \n The peasant dies for 1 shot for losing the peasant is taken away 1 life(heart) \n c) For one killed soldier is charged 50 points. \n The warrior dies for 3 shots.For loss to the warrior, the whole life(all the remaining hearts) is taken away. \n d) For a stone is not charged a single point.\n It can be jumped or destroyed in 10 strokes. \n When a pebble has come to you, the hero is deprived of all life(all hearts).\n 2) Terms of completion of the game : \n Loss of all lives(all hearts)."));
+			app.draw(txt2);
+			app.display();
+		}
+	}
 	return 0;
 }
